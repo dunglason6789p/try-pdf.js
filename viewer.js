932c450d867a5567e2366a6b017c057290d84e7f
son.nguyen2 <son.nguyen2@drjoy.vn>
2021-04-04 20:23:11 +0700

    1b

--- viewer.js
+++ viewer.js
@@ -206,7 +206,7 @@ const defaultOptions = {
     kind: OptionKind.WORKER
   },
   workerSrc: {
-    value: "../build/pdf.worker.js",
+    value: "./pdf.worker.js",
     kind: OptionKind.WORKER
   }
 };
